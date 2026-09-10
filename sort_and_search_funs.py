8ac426266bec0e56fc5cbf79338cd25eb05e86e4
Lawrence <law.fung.7@gmail.com>
2026-09-10 08:20:03 -0700

    test

--- sort_and_search_funs.py
+++ sort_and_search_funs.py
@@ -1,20 +1,21 @@
 
-def selection_sort_iter()
-    
+# Function to sort array using insertion sort
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
 
 
-
-
-
-
-
-
-
-
-
-
-#selection_sort_iter()
+#insert_sort_iter()
 
 
 
